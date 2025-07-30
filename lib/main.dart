1322a21aa2668a27036788bb9480e96376bea34d
mumarishtiaq <104498678+mumarishtiaq@users.noreply.github.com>
2025-07-30 11:31:05 -0700

    Initial - Upload

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EcommerceApp());
+}
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
